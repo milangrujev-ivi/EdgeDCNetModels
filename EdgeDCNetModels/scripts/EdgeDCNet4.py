3554f224dda20690f5a535ad6f7a6f6061cf006c
milangrujev-ivi <milan.grujev@ivi.ac.rs>
2025-11-16 18:43:55 +0100

    Update EdgeDCNet4.py

--- EdgeDCNetModels/scripts/EdgeDCNet4.py
+++ EdgeDCNetModels/scripts/EdgeDCNet4.py
@@ -1,8 +1,8 @@
 import keras
 
-def getEdgeDCNet4():
+def getEdgeDCNet4(input_shape=(160,160,3), activation='sigmoid', loss='binary_crossentropy', optimizer='adam'):
     model = keras.models.Sequential()
-    model.add(keras.layers.Input(shape=(160,160,3), name='input'))
+    model.add(keras.layers.Input(shape=input_shape, name='input'))
 
     model.add(keras.layers.Conv2D(3,(3,3),padding='valid',use_bias=False,strides=(2,2)))
 
@@ -34,10 +34,10 @@ def getEdgeDCNet4():
     model.add(keras.layers.Dropout(0.1))
 
     model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(2, activation='sigmoid'))
+    model.add(keras.layers.Dense(2, activation=activation))
 
-    model.compile(optimizer='adam',
-                  loss='binary_crossentropy',
+    model.compile(optimizer=optimizer,
+                  loss=loss,
                   metrics=['accuracy', keras.metrics.Precision(), keras.metrics.Recall()])
 
     #model.summary()
